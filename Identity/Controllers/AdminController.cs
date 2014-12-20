bbcd2faee8fcfc2047c3f4963e1a5f64638acf95
Aviv Shafir <aviv@optimalfinance.co.il>
2014-12-20 15:10:36 +0200

    added delete user option

--- Identity/Controllers/AdminController.cs
+++ Identity/Controllers/AdminController.cs
@@ -41,6 +41,28 @@ namespace Identity.Controllers
             return View(model);
         }
 
+        [HttpPost]
+        public async Task<ActionResult> Delete(string id)
+        {
+            AppUser user = await UserManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                IdentityResult result = await UserManager.DeleteAsync(user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View("Error", result.Errors);
+                }
+            }
+            else
+            {
+                return View("Error", new string[] { "User Not Found" });
+            }
+        }
+
         private void AddErrorsFromResult(IdentityResult result)
         {
             foreach (string error in result.Errors)
