086ab55920aa98c6facc83de0c3d521665d8a3e0
Aviv Shafir <aviv@optimalfinance.co.il>
2014-12-20 14:37:41 +0200

    added admin controller and views

---
+++ Identity/Infastructure/AppIdentityDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System.Data.Entity;
+using Identity.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Identity.Infastructure
+{
+    public class AppIdentityDbContext : IdentityDbContext<AppUser>
+    {
+        public AppIdentityDbContext() : base("IdentityDb")
+        {
+        }
+
+        static AppIdentityDbContext()
+        {
+            Database.SetInitializer<AppIdentityDbContext>(new IdentityDbInit());
+        }
+
+        public static AppIdentityDbContext Create()
+        {
+            return new AppIdentityDbContext();
+        }
+    }
+
+    public class IdentityDbInit : DropCreateDatabaseIfModelChanges<AppIdentityDbContext>
+    {
+        protected override void Seed(AppIdentityDbContext context)
+        {
+            PerformInitialSetup(context);
+            base.Seed(context);
+        }
+        public void PerformInitialSetup(AppIdentityDbContext context)
+        {
+            // initial configuration will go here
+        }
+    }
+}
---
+++ Identity/Models/AppUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Identity.Models
+{
+    public class AppUser : IdentityUser
+    {
+        // additional properties will go here
+    }
+}
